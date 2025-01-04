db97966849a4f19b3305626966bc2e263058bddf
irfanonk <onk.irfan@gmail.com>
2025-01-05 01:14:39 +0300

    update run.sh

--- run.sh
+++ run.sh
@@ -4,18 +4,23 @@ if [ "$1" == "stop-dev" ]; then
   echo "stoping docker-compose with docker-compose.dev.yaml"
   docker-compose -f docker-compose.dev.yaml down --remove-orphans -v
   exit 0
-else
-    if [ "$1" == "dev" ]; then
-  echo "Running docker-compose with docker-compose.dev.yaml"
-  docker-compose -f docker-compose.dev.yaml up --build
-else
-  echo "Running docker-compose with docker-compose.prod.yaml"
-  docker-compose -f docker-compose.prod.yaml up --build
-fi
+fi  
+
+if [ "$1" == "stop-prod" ]; then
   echo "stoping docker-compose with docker-compose.prod.yaml"
   docker-compose -f docker-compose.prod.yaml down --remove-orphans -v
   exit 0
 fi
 
 
+if [ "$1" == "dev" ]; then
+  echo "Running docker-compose with docker-compose.dev.yaml"
+  docker-compose -f docker-compose.dev.yaml up --build
+  exit 0
+fi
 
+if [ "$1" == "prod" ]; then
+  echo "Running docker-compose with docker-compose.prod.yaml"
+  docker-compose -f docker-compose.prod.yaml up --build
+  exit 0
+fi
